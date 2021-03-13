5dc0c44e4fe229c3c146744e0a74397cbe225b7e
Ahnaf An Nafee <48362866+ahnafnafee@users.noreply.github.com>
2021-03-13 18:42:26 -0500

    wanna die v2

--- temp.py
+++ temp.py
@@ -50,27 +50,34 @@ def train_test_split(data, train_size, random_state):
     dataset_tt = data_test[:, :-1]
 
     # Filtering features with low std
-    # dataset_tr = std_filter(dataset_tr, 0)
-    # dataset_tt = std_filter(dataset_tt, 0)
+    # dataset_tr = std_filter1(dataset_tr, 0)
+    # dataset_tt = std_filter1(dataset_tt, 0)
+
+    og_mean = np.mean(dataset_tr)
+    og_std = np.std(dataset_tr)
 
     # dataset_tr = (dataset_tr - np.mean(dataset_tr)) / np.std(dataset_tr)
     # dataset_tt = (dataset_tt - np.mean(dataset_tt)) / np.std(dataset_tt)
 
+    dataset_tr = (dataset_tr - og_mean) / og_std
+    dataset_tt = (dataset_tt - og_mean) / og_std
+
     # x_tr = (x_tr - np.mean(x_tr)) / np.std(x_tr)
     # x_tt = (x_tt - np.mean(x_tt)) / np.std(x_tt)
 
-    return x_tr, y_tr, x_tt, y_tt
-    # return dataset_tr, class_label_tr, dataset_tt, class_label_tt
+    # return x_tr, y_tr, x_tt, y_tt
+    return dataset_tr, class_label_tr, dataset_tt, class_label_tt
+    # return dataset_tr, y_tr, dataset_tt, y_tt
 
 
 def std_filter(data, std_val):
     '''Filters out features with low std'''
 
     # Standardizing the matrix
-    standardized_mat = (data - np.mean(data)) / np.std(data)
-
-    std_mat = np.std(standardized_mat, axis = 0)
-    col_num = standardized_mat.shape[1]
+    
+    temp_data = np.copy(data)
+    std_mat = np.std(temp_data, axis = 0)
+    col_num = temp_data.shape[1]
     low_idx = []
 
     for i in range(col_num):
@@ -79,11 +86,27 @@ def std_filter(data, std_val):
 
     temp = 0
     for j in low_idx:
-        standardized_mat = np.delete(standardized_mat, j - temp, 1)
+        temp_data = np.delete(temp_data, j - temp, 1)
         temp += 1
-
+    
+    standardized_mat = (temp_data - np.mean(temp_data)) / np.std(temp_data)
     return standardized_mat
 
+def std_filter1(data, std_val):
+    '''Filters out features with low std'''
+
+    
+    dataset = np.copy(data)
+    x = 0
+    while x < dataset.shape[1]:
+        if(np.std(dataset[:,x]) == 0):
+            dataset = np.delete(dataset,x,1)
+            x = x - 1
+        else:
+            dataset[:,x] = (dataset[:,x] - np.mean(dataset[:,x])) / np.std(dataset[:,x])
+            x = x + 1
+
+    return dataset
 
 
 class ClassifierEvaluation:
@@ -94,40 +117,48 @@ class ClassifierEvaluation:
         self.y_true_dict = dict(zip(unique, counts))
         unique, counts = np.unique(y_pred, return_counts=True)
         self.y_pred_dict = dict(zip(unique, counts))
+        
+
+    def eval(self):
         self.TP = 0
         self.TN = 0
         self.FP = 0
         self.FN = 0
 
-    def eval(self):
         for i in range(len(self.y_true)):
-            if (self.y_true[i] == self.y_pred[i] == 1):
+            if (self.y_true[i] == 1 and self.y_pred[i] == 1):
                 self.TP += 1
-            elif (self.y_true[i] == self.y_pred[i] == 0):
+            elif (self.y_true[i] == 1 and self.y_pred[i] == 0):
+                self.FP += 1
+            elif (self.y_true[i] == 0 and self.y_pred[i] == 1):
+                self.FN += 1
+            elif (self.y_true[i] == 0 and self.y_pred[i] == 0):
                 self.TN += 1
-        self.FP = len(self.y_true) - self.TP
-        self.FN = len(self.y_true) - self.TN
-        print(self.TP, self.TN, self.FP, self.FN)
+            
+
+        print()
+        print(f"Actual Samples: {self.y_true_dict}")
+        print(f"Predicted Samples: {self.y_pred_dict}")
+        print(f"TP: {self.TP}, TN: {self.TN}, FP: {self.FP}, FN: {self.FN}")
 
     def get_precision(self):
+        # print(f"Precision = {self.TP}/({self.TP}+{self.FP})")
         precision = self.TP/(self.TP + self.FP)
         return precision
 
     def get_recall(self):
+        # print(f"Recall = {self.TP}/({self.TP}+{self.FN})")
         recall = self.TP/(self.TP + self.FN)
         return recall
 
     def get_fmeasure(self):
         fmeasure = (2 * self.get_precision() * self.get_recall())/(self.get_precision() + self.get_recall())
-        return recall
+        return fmeasure
 
     def get_accuracy(self):
         accuracy = (self.TP + self.TN) /(self.TP + self.TN + self.FP + self.FN)
 
         return accuracy
-
-
-
 
 
 class NaiveBayes:
@@ -198,27 +229,42 @@ class NaiveBayes:
 
 
     def get_stats(self):
-        return self.mean_data, self.std_data, self.prior_data
+        # return np.sum(self.features.flatten())
+        print(self.mean_data.shape)
+        return np.sum(self.mean_data[:,0].flatten()), np.sum(self.mean_data[:,1].flatten())
 
     
     def calc_posterior(self, x):
-        '''Calculates posterior prob for each class'''
+        '''Chooses the class label based on which class probability is higher'''
 
         posteriors = []
 
         for i in range(self.n_classes):
             prior = np.log(self.prior_data[i])
             n_log = np.log(self.norm_pdf(x, i))
-            n_log = np.nan_to_num(n_log, nan=10^-8, posinf=10^8, neginf=10^-8)
+            n_log = np.nan_to_num(n_log, nan=10^-8, posinf=10^8, neginf=10^-14)
             posterior = np.sum(n_log)
             posterior = prior + posterior
             posteriors.append(posterior)
         return self.classes[np.argmax(posteriors)]
 
 
+    def calc_posterior1(self, x):
+        '''Calculates posterior prob for each class'''
+
+        posteriors = []
+
+        for i in range(self.n_classes):
+            prior = self.prior_data[i]
+            n_pdf = self.norm_pdf(x, i)
+            n_pdf = np.prod(np.nan_to_num(n_pdf, nan=10^-8, posinf=10^8, neginf=10^-8))
+            posterior = prior * n_pdf
+            posteriors.append(posterior)
+        return self.classes[np.argmax(posteriors)]
+
+
     def predict(self, x):
-        preds = [self.calc_posterior(i) for i in x]
-        # preds = [self.calc_posterior(i) for i in x[:5]]
+        preds = [self.calc_posterior1(i) for i in x]
         return np.asarray(preds, dtype=np.float64)
 
 
@@ -228,7 +274,7 @@ class NaiveBayes:
         mean = self.mean_data[c_idx]
         std = self.std_data[c_idx]
 
-        numerator = np.exp((-1/2)*((data-mean)**2) / (2 * (std**2)))
+        numerator = np.exp(- (data-mean)**2 / (2 * (std**2)))
         denominator = std * np.sqrt(2 * np.pi)
 
         return numerator / denominator
@@ -238,17 +284,20 @@ print(type(y_tt))
 
 g_nb = NaiveBayes(x_tr, y_tr)
 
-# g_nb.class_sep()
-g_nb.fit()
-print(g_nb.get_target())
-# print(g_nb.get_stats())
+g_nb.class_sep()
+# g_nb.fit()
+# print(g_nb.get_target())
+print(g_nb.get_stats())
 
 predictions = g_nb.predict(x_tt)
-print("LEN: ", len(predictions))
+print("LEN: ", predictions)
 gb_ce = ClassifierEvaluation(y_tt, predictions)
 gb_ce.eval()
 # gb_ce.get_accuracy()
-print("Naive Bayes classification accuracy", gb_ce.get_accuracy())
+print("Precision:", gb_ce.get_precision())
+print("Recall:", gb_ce.get_recall())
+print("F-measure", gb_ce.get_fmeasure())
+print("Accuracy:", gb_ce.get_accuracy())
 print()
 print()
 # g_nb.predict(x_tt)
